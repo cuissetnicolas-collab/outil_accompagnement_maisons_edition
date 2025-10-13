4524651b26f694da889806dbe38064550a63582c
cuissetnicolas-collab <cuisset.nicolas@gmail.com>
2025-10-13 11:11:44 +0200

    Update application.py

--- application.py
+++ application.py
@@ -11,7 +11,9 @@ if "login" not in st.session_state:
     st.session_state["login"] = False
 
 def login(username, password):
-    users = {"aurore": {"password": "12345", "name": "Aurore Demoulin"}}
+    users = {"aurore": {"password": "12345", "name": "Aurore Demoulin"},
+            {"laure.froidefond": {"password": "Laure2019$", "name": "Laure Froidefond"}
+            }
     if username in users and password == users[username]["password"]:
         st.session_state["login"] = True
         st.session_state["username"] = username
